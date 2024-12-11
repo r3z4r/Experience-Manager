9f0023c340cab48fad1d0fc83165a06d1bbae081
REZA <mohamareza.rohani@tecnotree.com>
2024-12-11 22:29:01 +0530

    adding .env file for deployment

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/xpm',
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['demo.tecnotree.com', 'localhost:5200'],
+    },
+  },
+  env: {
+    NEXT_PUBLIC_SITE_URL: 'https://demo.tecnotree.com',
+  },
 }
 
 export default withPayload(nextConfig)
