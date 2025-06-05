fc3e6b6edfab3d943096b19a96b250f60b6ae298
REZA <mohamareza.rohani@tecnotree.com>
2025-06-05 18:10:23 +0530

    auth improvment with built 0n payloadcms

--- collections/Users.ts
+++ collections/Users.ts
@@ -2,10 +2,14 @@ import type { CollectionConfig } from 'payload'
 
 export const Users: CollectionConfig = {
   slug: 'users',
+  auth: true,
   admin: {
     useAsTitle: 'email',
   },
-  auth: true,
+  access: {
+    create: () => true,
+    read: () => true,
+  },
   fields: [
     {
       name: 'username',
